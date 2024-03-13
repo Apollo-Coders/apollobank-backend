6acc21f8cb37e1e31f0453ba917380a7887c4d90
Brenda Aniceto Bueno <brendabuenobrito@gmail.com>
2024-03-13 16:45:17 -0300

    Added signatures to TransactionsRepository methods

--- Repositories/Interfaces/ITransactionsRepository.cs
+++ Repositories/Interfaces/ITransactionsRepository.cs
@@ -4,7 +4,7 @@ namespace ApolloBank.Repositories.Interfaces
 {
     public interface ITransactionsRepository
     {
-        Task<IEnumerable<Transaction>> GetAllTransactions();
+        Task<IEnumerable<Transaction>> GetAllTransactions(int? id);
         Task<Transaction> GetLastSixMonthsTransactions(int? id);
         Task<Transaction> GetCurrentMonthTransactions(int? id);
         Task<Transaction> AddTransaction(Transaction Transactionto, Transaction TransactionFrom = null);
--- Repositories/TransactionsRepository.cs
+++ Repositories/TransactionsRepository.cs
@@ -1,8 +1,28 @@
 ﻿using ApolloBank.Repositories.Interfaces;
+using System.Transactions;
 
 namespace ApolloBank.Repositories
 {
     public class TransactionsRepository : ITransactionsRepository
     {
+        public Task<Transaction> AddTransaction(Transaction Transactionto, Transaction TransactionFrom = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Transaction>> GetAllTransactions(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Transaction> GetCurrentMonthTransactions(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Transaction> GetLastSixMonthsTransactions(int? id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
