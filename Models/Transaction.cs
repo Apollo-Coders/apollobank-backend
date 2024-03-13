b6287ec43b4c739a28ef8d3300b0b3d9727148f9
Brenda Aniceto Bueno <brendabuenobrito@gmail.com>
2024-03-13 11:47:48 -0300

    Assigning attributes and constructor to the Transaction class

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -2,5 +2,26 @@
 {
     public class Transaction
     {
+        public int Id { get; private set; }
+        public int Amount { get; private set; }
+        public string? To { get; private set; }
+        public string? From { get; private set; }
+        public DateTime Date { get; private set; }
+        public string Description { get; private set; }
+        //Criar Enuns
+        public string Transaction_Type { get; private set; }
+        public int Account_Id { get; set; }
+
+        public Transaction(int id, int amount, string? to, string? from, DateTime date, string description, string transaction_Type, int account_Id)
+        {
+            Id = id;
+            Amount = amount;
+            To = to;
+            From = from;
+            Date = date;
+            Description = description;
+            Transaction_Type = transaction_Type;
+            Account_Id = account_Id;
+        }
     }
 }
