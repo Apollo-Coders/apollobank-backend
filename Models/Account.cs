14631740b774dd87726649e88fd16cc07bef98b2
Daiane Bolzan <dainha85@gmail.com>
2024-03-13 17:00:34 -0300

    feat: user, adress e account models

--- Models/Account.cs
+++ Models/Account.cs
@@ -2,5 +2,18 @@
 {
     public class Account
     {
+        public int Id { get; set; }
+        public int Balance { get; set; }
+        public string AccountNumber { get; set; }=null!;
+        public CreditCards? CreditCards { get; set; }
+        public Transactions? Transactions { get; set; }
+
+        public float CreditLimit { get; set; }
+        public List<CreditCard>? CreditCard { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }= null!;
+
+
     }
 }
--- Models/Address.cs
+++ Models/Address.cs
@@ -2,5 +2,13 @@
 {
     public class Address
     {
+        public int Id { get; set; }
+        public string Street { get; set; } = string.Empty;
+        public int Number { get; set; }
+        public string? Complement { get; set; }
+        public string Neighborhood { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public bool Policies { get; set; }
     }
 }
