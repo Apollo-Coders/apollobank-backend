56e66daef5c342c0014cc3d68d73dea45d642df7
Guilherme Duarte <guilhduart.abr@gmail.com>
2024-03-13 10:10:39 -0300

    Set dbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ApolloBank.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApolloBank.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<CreditCards> CreditCardsSet { get; set;}
+        public DbSet<CreditCard> CreditCards { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("DataSource=app.db;Cache=shared");
+        }
+
+    }
+}
