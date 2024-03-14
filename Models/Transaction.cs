2a03070d7773218ce194cb49de18044d1d13576d
Brenda Aniceto Bueno <brendabuenobrito@gmail.com>
2024-03-14 00:32:39 -0300

    Including the value to the transaction date and IEntityTypeConfiguration the transaction class.

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,4 +1,6 @@
 ﻿using ApolloBank.Enums;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApolloBank.Models
 {
@@ -8,7 +10,7 @@ namespace ApolloBank.Models
         public double Amount { get; private set; }
         public string? To { get; private set; }
         public string? From { get; private set; }
-        public DateTime Date { get; private set; }
+        public DateTime Date { get; private set; } = DateTime.Now;
         public string? Description { get; private set; }
         public TransactionType Transaction_Type { get; private set; }
         public double Direction { get; private set; }
@@ -25,6 +27,20 @@ namespace ApolloBank.Models
             Transaction_Type = transaction_Type;
             Direction = direction;
             Account_Id = account_Id;
+        }
+    }
+
+
+
+    public class TransactionMap : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transactions");
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Amount).IsRequired().HasPrecision(10, 2); 
+            builder.Property(u => u.Description).HasMaxLength(100).HasColumnType("varchar(100)");
+            builder.Property(u => u.Transaction_Type).IsRequired();
         }
     }
 }
