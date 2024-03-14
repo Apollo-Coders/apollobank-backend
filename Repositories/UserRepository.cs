b751c9eb6e0baaf26345e796dca4ef3cff6ecf36
Yasmin Gomes <yasmin.viieira16@gmail.com>
2024-03-14 07:31:11 -0300

    Adding interface and user repository.

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,8 +1,39 @@
-﻿using ApolloBank.Repositories.Interfaces;
+﻿using ApolloBank.Models;
+using ApolloBank.Repositories.Interfaces;
+using System;
 
 namespace ApolloBank.Repositories
 {
     public class UserRepository : IUserRepository
     {
+        public Task<User> CreateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByCPF(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
