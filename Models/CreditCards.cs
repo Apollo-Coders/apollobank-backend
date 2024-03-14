9fac31faf97f54ff9f6a96951af0ef195c08e4ab
caiohxp <caiocesar.ccomp@gmail.com>
2024-03-14 20:03:21 -0300

    create methods in CreditCards class

--- Models/CreditCards.cs
+++ Models/CreditCards.cs
@@ -5,11 +5,31 @@
         public float TotalCreditLimit { get; private set; }
         public float TotalCreditUsed { get; private set; }
         public float TotalAlocatedCredit { get; private set; }
-        public CreditCards(float totalCreditLimit, float totalCreditUsed, float totalAlocatedCredit){
+        public List<CreditCard> creditCards = new List<CreditCard>();
+        public CreditCards(float totalCreditLimit, float totalCreditUsed, float totalAlocatedCredit)
+        {
             TotalCreditLimit = totalCreditLimit;
             TotalCreditUsed = totalCreditUsed;
             TotalAlocatedCredit = totalAlocatedCredit;
         }
+
+        public void createCreditCard(CreditCard card)
+        {
+            creditCards.Add(card);
+        }
+        public void blockCreditCard(int cardId)
+        {
+            CreditCard cardToRemove = creditCards.FirstOrDefault(x => x.Id == cardId);
+            if (cardToRemove != null)
+            {
+                creditCards.Remove(cardToRemove);
+                Console.WriteLine($"Credit Card com ID {cardId} removido com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine($"Credit Card com ID {cardId} não encontrado.");
+            }
+        }
     }
-    
+
 }
