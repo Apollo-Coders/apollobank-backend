d9f0ba5ffffedb84592015336befa7dbacaf4d32
caiohxp <caiocesar.ccomp@gmail.com>
2024-03-13 23:45:46 -0300

    create InvoiceStatus model and up CreditCard, CreditCards and Invoice models

--- Models/CreditCard.cs
+++ Models/CreditCard.cs
@@ -2,5 +2,24 @@
 {
     public class CreditCard
     {
+        public int Id { get; private set; }
+        public int Number { get; private set; }
+        public int Cvc { get; private set; }
+        public DateTime ExpirationTime { get; private set; }
+        public double CreditUsed { get; private set; }
+        public double CreditLimit { get; private set; }
+
+        public CreditCard(int id, int number, int cvc, DateTime expirationTime, double creditUsed, double creditLimit){
+            Id = id;
+            Number = number;
+            Cvc = cvc;
+            ExpirationTime = expirationTime;
+            CreditUsed = creditUsed;
+            CreditLimit = creditLimit;
+        }
+
+        public void setLimit(float limit){
+            CreditLimit = limit;
+        }
     }
 }
--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -2,5 +2,20 @@
 {
     public class Invoice
     {
+        public int Id { get; private set; }
+        public DateTime InvoiceDate { get; private set; }
+        public double InvoiceTotalAmount { get; private set; }
+        public double InvoicePaid { get; private set; }
+
+        public Invoice(int id, DateTime invoiceDate, double invoiceTotalAmount, double invoicePaid){
+            Id = id;
+            InvoiceDate = invoiceDate;
+            InvoiceTotalAmount = invoiceTotalAmount;
+            InvoicePaid = invoicePaid;
+        }
+
+        public void payInvoice(double amount){
+            InvoicePaid = amount;
+        }
     }
 }
