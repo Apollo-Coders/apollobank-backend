350575a17878ac61fdd095d7944bbfc0b9a882e1
luizaferreirafonseca <luiza.ferreira.fonseca@gmail.com>
2024-03-15 07:44:32 -0300

    adding interfaces and repository

--- Repositories/CreditCardRepository.cs
+++ Repositories/CreditCardRepository.cs
@@ -1,8 +1,33 @@
-﻿using ApolloBank.Repositories.Interfaces;
+﻿using ApolloBank.Models;
+using ApolloBank.Repositories.Interfaces;
 
 namespace ApolloBank.Repositories
 {
     public class CreditCardRepository: ICreditCardRepository
     {
+        public Task<CreditCard> CreateCreditCard(CreditCard creditCard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CreditCard> BlockCreditCard(int cardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetLimit(float limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddTransactionToInvoice(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> VerifyCreditLimit(int cardNumber)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Repositories/Interfaces/ICreditCardRepository.cs
+++ Repositories/Interfaces/ICreditCardRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace ApolloBank.Repositories.Interfaces
+﻿using ApolloBank.Models;
+
+namespace ApolloBank.Repositories.Interfaces
 {
     public interface ICreditCardRepository
     {
+        Task<CreditCard> CreateCreditCard(CreditCard creditCard);
+        Task<CreditCard> BlockCreditCard(int cardId); 
+
+        Task SetLimit(float limit);
+
+        Task AddTransactionToInvoice(Transaction transaction);
+
+        Task<bool> VerifyCreditLimit(int cardNumber); 
+
     }
 }
