c6c9c2f404b8c0efbe3b20fef10f907161552562
LucasViniciuus <lucas.vinicius03@outlook.com>
2024-03-17 12:36:01 -0300

    feat: Interface that defines contracts for authentication methods

--- Services/Interfaces/IAuthService.cs
+++ Services/Interfaces/IAuthService.cs
@@ -1,6 +1,14 @@
-﻿namespace ApolloBank.Services.Interfaces
+﻿using ApolloBank.DTOs;
+using ApolloBank.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApolloBank.Services.Interfaces
 {
     public interface IAuthService
     {
+        Task<bool> FoundExistingUser(string cpf);
+        bool AuthenticateAsync(string email, string senha);
+        public string GenerateToken(User user);
+        
     }
 }
