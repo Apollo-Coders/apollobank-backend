3cccd8e8cfd00c395e511ab42c4cf5dae45b1360
Daiane Bolzan <dainha85@gmail.com>
2024-03-17 12:55:26 -0300

    fix: remove unused imports

--- DTOs/BaseUserDTO.cs
+++ DTOs/BaseUserDTO.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 namespace ApolloBank.DTOs
 {
     public class BaseUserDTO
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+﻿
 using ApolloBank.Data;
 using ApolloBank.DTOs;
 using ApolloBank.Models;
