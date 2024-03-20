4100bc60d67e95163965008e1bc27ca995e830e1
luizaferreirafonseca <luiza.ferreira.fonseca@gmail.com>
2024-03-20 06:47:53 -0300

    data annotations on  creditcarddetaisldto

--- DTOs/CreditCardDetailsDTO.cs
+++ DTOs/CreditCardDetailsDTO.cs
@@ -1,11 +1,19 @@
-﻿namespace ApolloBank.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApolloBank.DTOs
 {
     public class CreditCardDetailsDTO
     {
+
+        [Key]
         public int Id { get; set; }
         public bool IsBlocked { get; set; }
         public string Number { get; set; }
+
+        [Range(100, 999)]
         public int Cvc { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime ExpirationTime { get; set; }
         public double CreditUsed { get; set; }
         public double CreditLimit { get; set; }
