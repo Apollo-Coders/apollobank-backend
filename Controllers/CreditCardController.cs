8bd0c9d2deaf1647786deabb4679dd8306b1758b
Vítor Moraes <vitorpatrick@gmail.com>
2024-03-20 22:32:58 -0300

    Create CreditCardController endpoints

--- Controllers/CreditCardController.cs
+++ Controllers/CreditCardController.cs
@@ -1,5 +1,6 @@
 ﻿using ApolloBank.DTOs;
 using ApolloBank.Migrations;
+using ApolloBank.Models;
 using ApolloBank.Repositories.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -45,6 +46,41 @@ namespace ApolloBank.Controllers
                     return NotFound();
                 }
                 return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("{amount}/{accountid}")]
+        public async Task<IActionResult> AddAmountToTotalLimit(double amount, int accountId)
+        {
+            try
+            {
+                await _creditCardsRepository.AddAmountToTotalLimit(amount, accountId);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{accountid}")]
+        public async Task<IActionResult> GetCreditCardsByAccountId(int accountId)
+        {
+            try
+            {
+                var creditCard = await _creditCardsRepository.GetCreditCardsByAccountId(accountId);
+                if (creditCard == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(creditCard);
+                }
             }
             catch (Exception ex)
             {
