e56dababd84d88f14e2510bb9c328c223ddd0271
Guilherme Duarte <guilhduart.abr@gmail.com>
2024-03-21 08:51:02 -0300

    fix: Invoice constructor

--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -9,15 +9,18 @@ namespace ApolloBank.Models
         public double InvoiceTotalAmount { get; set; }
         public double InvoicePaid { get; set; }
 
-        public InvoiceStatus status { get; private set; }
+        public InvoiceStatus Status { get; private set; }
 
         public int? AccountId { get; set; }
         public Account? Account { get; set; }
 
+        public Invoice() { }
         public Invoice(DateTime invoiceDate, int accountId){
             InvoiceDate = invoiceDate;
             InvoiceTotalAmount = 0.0d;
             InvoicePaid = 0.0d;
+            Status = InvoiceStatus.PENDING;
+            AccountId = accountId;
         }
 
        
