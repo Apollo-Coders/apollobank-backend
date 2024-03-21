3a459be611f00502028603f02e2a6484d62439d7
Vítor Moraes <vitorpatrick@gmail.com>
2024-03-20 22:25:22 -0300

    Create endpoint at CreditCardController

---
+++ Controllers/InvoiceController.cs
@@ -0,0 +1,23 @@
+﻿using ApolloBank.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApolloBank.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+       private readonly IInvoiceRepository _invoiceRepository;
+
+        public InvoiceController(IInvoiceRepository invoiceRepository)
+        {
+            _invoiceRepository = invoiceRepository;
+        }
+
+        [HttpGet("{id}")]
+        public Task<IActionResult> Get
+        {
+           
+        }
+    }
+}
